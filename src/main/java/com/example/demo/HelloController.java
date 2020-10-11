781c1fc5c02918beacf86ef8a8d5c3b8737bd18e
Trung Doan <doanptand@gmail.com>
2020-10-11 08:28:08 +0700

    get mapping return json

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import com.example.demo.entities.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class HelloController {
     @RequestMapping("/")
     public String hello() {
         return "Welcome to Spring";
+    }
+
+    @GetMapping("/user")
+    public User getOneUser() {
+        return new User(1,"doanpt","doandeptrai", "admin");
     }
 }
