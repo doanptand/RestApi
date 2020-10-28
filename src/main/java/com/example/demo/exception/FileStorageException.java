55168ca922f2642842a613464e8f3bc98bed7899
Trung Doan <v.doanpt1@vinsmart.net.vn>
2020-10-28 21:16:31 +0700

    auto wired file properties and create exceptions

---
+++ src/main/java/com/example/demo/exception/FileStorageException.java
@@ -0,0 +1,17 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class FileStorageException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
